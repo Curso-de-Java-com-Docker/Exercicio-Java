7c94fc0572364e8557e51f5e9603c9d308be60c8
root <root@Estudos>
2024-11-11 07:12:39 -0300

    Innitial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+org.dejailton.sistemaregistrador
+
+O software serve para registrar pacientes de uma clinica.
---
+++ src/test/java/org/dejailton/MainTest.java
@@ -0,0 +1,9 @@
+package org.dejailton.sistemaregistrador;
+import org.dejailton.sistemaregistrador.Main;
+
+public class MainTest {
+	public static void main (String[] args) {
+		Main.main(new String[4]);	
+	}
+}
+
