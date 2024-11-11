00bd965e1c5553103c1571ba4b4d91db987ae40d
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2024-11-11 10:47:18 -0300

    make println

--- src/test/java/org/dejailton/sistemaregistrador/MainTest.java
+++ src/test/java/org/dejailton/sistemaregistrador/MainTest.java
@@ -4,7 +4,9 @@ import org.dejailton.sistemaregistrador.ClientTest;
 
 public class MainTest {
 	public static void main (String[] args) {
+		System.out.println("Starting Automated Tests...");
 		ClientTest.test();
+		System.out.println("End of Automated Tests...");
 	}
 }
 
