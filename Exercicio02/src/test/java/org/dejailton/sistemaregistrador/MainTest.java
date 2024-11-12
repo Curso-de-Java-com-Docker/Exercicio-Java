44319c3185b0f4dd1c829f87ccaf876c33c57f15
dejailton <devdejailton@gmail.com>
2024-11-12 06:21:49 -0300

    Criado outra versão do software na pasta Exercicio02

---
+++ Exercicio02/src/test/java/org/dejailton/sistemaregistrador/MainTest.java
@@ -0,0 +1,12 @@
+package org.dejailton.sistemaregistrador;
+
+import org.dejailton.sistemaregistrador.ClientTest;
+
+public class MainTest {
+	public static void main (String[] args) {
+		System.out.println("Starting Automated Tests...");
+		ClientTest.test();
+		System.out.println("End of Automated Tests...");
+	}
+}
+
