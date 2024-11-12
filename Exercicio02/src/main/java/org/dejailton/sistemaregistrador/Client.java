59ce2ca57c78081be36952c470999c50ba9b4d97
dejailton <devdejailton@gmail.com>
2024-11-12 06:46:06 -0300

    Atualização das classes Client e ClientTest

--- Exercicio02/src/main/java/org/dejailton/sistemaregistrador/Client.java
+++ Exercicio02/src/main/java/org/dejailton/sistemaregistrador/Client.java
@@ -1,5 +1,23 @@
 package org.dejailton.sistemaregistrador;
 
 public class Client {
+	private String name;
+	private String cpf;
+	private int bornYear;
+
+	public Client (String name, String cpf, int bornYear) {
+		this.name = name;
+		this.cpf = cpf;
+		this.bornYear = bornYear;
+	}
+	public String getName() {
+		return this.name;
+	}
+	public String getCpf() {
+		return this.cpf;
+	}
+	public int getYear() {
+		return this.bornYear;
+	}
 }
 
