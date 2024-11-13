23410282d6a5602273f202b8be5ba35dc58ea249
dejailton <devdejailton@gmail.com>
2024-11-12 21:28:17 -0300

    Changed class's LiteDriver, PatientNULL

---
+++ Exercicio04/src/main/java/org/dejailton/sistemaregistrador/LiteDriver.java
@@ -0,0 +1,54 @@
+package org.dejailton.sistemaregistrador;
+import org.dejailton.sistemaregistrador.Patient;
+import java.sql.Connection;
+import java.sql.Driver manager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class LiteDriver {
+
+	public static void registerPatient(Patient patient) {
+		Connection connection = null;
+		try {
+			connection = DriverManager.getConnection("jdbc:sqlite:base.db");
+			Statement statement = connection.createStatement();
+			statement = setQueryTimeout(30);
+
+			statement.executeUpdate("CREATE TABLE patienttable(name STRING, cpf STRING, birthdate String, gender char");
+			statement.executeUpdate("INSERT INTO patienttable VALUES(patient.getName(), patient.getCpf(), patient.getBirthDate(), patient.getGender()");
+		} catch(SQLException e) {
+			System.err.println(e.getMessage());
+		}
+		finally {
+			try {
+				if(connection != null) {
+					connection.close();
+				}
+			} catch(SQLException e) {
+				System.err.println(e.getMessage());
+			}
+		}
+	}
+	public static void getPatientByCpf(String wrongCpf) {
+		Connection connection = null;
+		try {
+			connection = DriverManager.getConnection("jdbc:sqlite:base.db");
+			Statement statement = connection.createStatement();
+			statement = setQueryTimeout(30);
+
+			statement.executeUpdate("SELECT cpf FROM patienttable WHERE cpf = 'wrongCpf'");
+		} catch(SQLException e) {
+			System.err.println(e.getMessage());
+		}
+		finally {
+			try {
+				if(connection != null) {
+					connection.close();
+				}
+			} catch(SQLException e) {
+				System.err.println(e.getMessage());
+			}
+		}
+	}
+}
