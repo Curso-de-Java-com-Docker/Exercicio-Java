b8df2ebab3bf8819b69e11d53f0f8af3c2efdfdc
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2024-11-12 21:52:06 -0300

    fix Driver.manager -> DriverManager

--- Exercicio04/src/main/java/org/dejailton/sistemaregistrador/LiteDriver.java
+++ Exercicio04/src/main/java/org/dejailton/sistemaregistrador/LiteDriver.java
@@ -1,7 +1,8 @@
 package org.dejailton.sistemaregistrador;
+
 import org.dejailton.sistemaregistrador.Patient;
 import java.sql.Connection;
-import java.sql.Driver.manager;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
