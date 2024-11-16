7276fe2a6c2bd811c22a9dbe7d9732bbe7c91323
root <root@Estudos>
2024-11-16 00:34:34 -0300

    Change class Controller and class Question

--- Exercicio06/src/main/java/org/dejailton/sistemaregistrador/Controller.java
+++ Exercicio06/src/main/java/org/dejailton/sistemaregistrador/Controller.java
@@ -2,73 +2,72 @@ package org.dejailton.sistemaregistrador;
 
 import org.dejailton.sistemaregistrador.Question;
 
-class Controller {
-	String option;
-	String name = null;
-	String cpf;
-	String birthdate;
-	char gender;
-	int lastquestion;
-	int response;
-	String questionresponse;
+public class Controller {
+	private int option;
+	private String name;
+	private String cpf;
+	private String birthdate;
+	private char gender;
+	private int lastquestion;
+	private int response;
+	private String questionresponse;
 	
 	public String init () {
 		return "Welcome to sistemaregistrador!";
 	}
 	public String nextQuestion () {
 		Question question = new Question();
-		String textquestion = "";
-		String questionresponse = "";
-		if (this.option == null) {
-			textquestion = "Select operation [1] Register [2] Query";
+		String  questionresponse = "";
+		String type = "";
+		if (this.option == 0) {
+			question.setText("Select operation [1] Register [2] Query");
+			question.setType("String"); //Future implementation.
+			questionresponse = question.getText();
+			this.option = 1;
 			lastquestion = 1;
 		} else if (this.name == null) {
 			question.setText("Name: ");
+			question.setType("String"); //Future implementation.
 			questionresponse = question.getText();	
 			lastquestion = 2;
 		} else if (this.cpf == null) {
 			question.setText("Cpf: ");
-			questionresponse = question.getText();	
+			question.setType("String"); //Future implementation.
+			questionresponse = question.getText();
 			lastquestion = 3;
 		} else if (this.birthdate == null) {
 			question.setText("Birth date: ");
+			question.setType("String"); //Future implementation.
 			questionresponse = question.getText();	
 			lastquestion = 4;
 		} else {
 			question.setText("Gender: ");
+			question.setType("char"); //Future implementation.
 			questionresponse = question.getText(); 			
 			lastquestion = 5;
 		}
-		if (lastquestion == 1) {
-			return textquestion;
-		} else {
-			return questionresponse;
-		}	
+		return questionresponse;	
 	}
-	public String answer (String answer) {
+	public void answer (String answer) {
 		if (lastquestion == 2) {
 			this.name = answer;
-			return name;
 		} else if (lastquestion == 3) {
 			this.cpf = answer;
-			return cpf;
 		} else if (lastquestion == 4) {
-			this.birthdate = birthdate;
-			return birthdate;
+			this.birthdate = answer;
 		} else {
-			return "Last question is not valid!";
+			System.out.println("Last question is not valid!");
 		}
 	}
-	public int answer (int answer) {
+	public void answer (int answer) {
 		if (lastquestion == 1) {
 			this.response = answer;
 		}
-		return response;
 	}	
-	public char answer (char answer) {
+	public void answer (char answer) {
 		if (lastquestion == 5) {
 			this.gender = answer;
 		}
-		return gender;
 	}
 }
+
--- Exercicio06/src/main/java/org/dejailton/sistemaregistrador/Question.java
+++ Exercicio06/src/main/java/org/dejailton/sistemaregistrador/Question.java
@@ -2,7 +2,12 @@ package org.dejailton.sistemaregistrador;
 
 class Question {
 	private String text;
+	private String type;
 
+	public Question (String text, String type) {
+		this.text = text;
+		this.type = type;
+	}
 	public Question (String text) {
 		this.text = text;
 	}
@@ -13,5 +18,11 @@ class Question {
 	}
 	public void setText(String text) {
 		this.text = text;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
 	}
 }
