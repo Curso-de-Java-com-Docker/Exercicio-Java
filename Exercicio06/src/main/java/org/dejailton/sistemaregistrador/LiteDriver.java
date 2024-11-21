c341522ec9de880611f5533402feba33e31db95a
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2024-11-20 23:55:46 -0300

    major rewrite

--- Exercicio06/src/main/java/org/dejailton/sistemaregistrador/LiteDriver.java
+++ Exercicio06/src/main/java/org/dejailton/sistemaregistrador/LiteDriver.java
@@ -9,73 +9,95 @@ import java.sql.Statement;
 
 public class LiteDriver {
 	public static boolean registerPatient(Patient patient) {
-		Connection connection = null;
-		try {
-			
-			String name1 = patientRegister.getName();
-			String cpf1 = patientRegister.getCpf();
-			String brithDate1 = patientRegister.getBirthDate();
-			char gender1 = patientRegister.getGender();
-			Class.forName("org.sqlite.JDBC");
-			success = true;
-			connection = DriverManager.getConnection("jdbc:sqlite:meubanco.db");
+		String insertString = getInsertString(patient);
+		boolean success = false;	// bota pra ser se deu certo ou nao depois
+		executeUpdate(insertString);
+		return success;
+	}
+
+	private static String getInsertString (Patient patient) {
+		String queryString;
+		String name = patient.getName();
+		String cpf = patient.getCpf();
+		///...
+		// queryString = String.format(...);
+		queryString = "";
+		return queryString;
+	}
+
+	private static void executeUpdate (String sqlString) {
+		ensureTable();
+		try (Connection connection = getConnection()) {
 			Statement statement = connection.createStatement();
 			statement.setQueryTimeout(30);
-			String createTable = "CREATE TABLE patienttable(name STRING, cpf STRING, birthdate STRING, gender CHAR";
-			statement.executeUpdate(createTable);
-			String Insert = "INSERT INTO patienttable VALUES(patient.getName(), patient.getCpf(), patient.getBirthDate(), patient.getGender()";
-			statement.executeUpdate();
-			success = true;
-		} catch(SQLException e) {
+			statement.executeUpdate(sqlString);
+		} catch (SQLException e) {
 			System.err.println(e.getMessage());
-			success = false;
-		} finally {
-			try {
-				if(connection != null) {
-					connection.close();
-				}
-			} catch(SQLException e) {
-				System.err.println(e.getMessage());
-			} 
-			return success;
 		}
+	}
+
+	private static void ensureTable () {
+		String tableName = "Patients";
+		String nameCol = "name STRING";
+		String cpfCol = "cpf STRING";
+		String birth = "birth STRING";
+		String gender = "gender STRING";
+		String preStatement = "CREATE TABLE IF NOT EXISTS %s (%s, %s, %s, %s);";
+		String statementString = String.format(preStatement, nameCol, cpfCol, birth, gender);
+		execute(statementString);
+	}
+
+	private static void execute (String sqlString) {
+		try (Connection connection = getConnection()) {
+			Statement statement = connection.createStatement();
+			statement.setQueryTimeout(30);
+			statement.execute(sqlString);
+		} catch (SQLException e) {
+			System.err.println(e.getMessage());
+		}
+	}
+
+	private static Connection getConnection () {
+		Connection connection;
+		Class.forName("org.sqlite.JDBC");
+		connection = DriverManager.getConnection("jdbc:sqlite:meubanco.db");
+		return connection;
 	}
 
 	public static Patient getPatientByCpf(String wrongCpf) {
-		Connection connection = null;
-		Patient patient;
-		try {
-			Class.forName("org.sqlite.JDBC");
-			connection = DriverManager.getConnection("jdbc:sqlite:meubanco.db");
+		String queryString(cpf);
+		executeQuery(queryString);
+	}
+
+	private static Patient queryString (String cpf) {
+		ensureTable();
+		try (Connection connection = getConnection()) {
 			Statement statement = connection.createStatement();
 			statement.setQueryTimeout(30);
-
-			ResultSet patientReturnSet = statement.executeQuery("SELECT cpf FROM patienttable WHERE cpf = 'wrongCpf'");
-			if (patientReturnSet.getFetchSize() == 1) {
-				patientReturnSet.next();
-				patientReturnSet.first();
-				String name = patientReturnSet.getString("name");
-				String cpf = patientReturnSet.getString("cpf");
-				String birthdate = patientReturnSet.getString("birthdate");
-				char gender = patientReturnSet.getString("gender").charAt(0);
-				patient = new Patient(name, cpf, birthdate, gender);
-			} else if (patientReturnSet.getFetchSize() > 1) {
-				throw new Exception("Expected an unique result for cpf, but got multiple");
-			} else {
-				throw new Exception("No results for this cpf");
-			}
-		} catch(Exception e) {
+			Result result = statement.executeQuery(sqlString);
+			Patient patient = resultToPatient(result);
+			return patient;
+		} catch (SQLException e) {
 			System.err.println(e.getMessage());
-			patient = new Patient();
-		} finally {
-			try {
-				if(connection != null) {
-					connection.close();
-				}
-			} catch(SQLException e) {
-				System.err.println(e.getMessage());
-			}
 		}
+	}
+
+	private static Patient resultToPatient (Result result) {
+		if (patientReturnSet.getFetchSize() > 1) {
+			throw new Exception("Expected an unique result for cpf, but got multiple");
+		} else if (patientReturnSet.getFetchSize() < 1) {
+			throw new Exception("No results for this cpf");
+		}
+		// troca patientReturnSet por result
+		Patient patient;
+		patientReturnSet.next();
+		patientReturnSet.first();
+		String name = patientReturnSet.getString("name");
+		String cpf = patientReturnSet.getString("cpf");
+		String birthdate = patientReturnSet.getString("birthdate");
+		char gender = patientReturnSet.getString("gender").charAt(0);
+		patient = new Patient(name, cpf, birthdate, gender);
 		return patient;
 	}
 }
+
